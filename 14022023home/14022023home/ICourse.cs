aa38f01e8deb70ecdb155cd4415764e6c50e0002
Nihat <nihattagizade2003@gmail.com>
2023-02-14 02:56:42 +0400

    d

---
+++ 14022023home/14022023home/ICourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14022023home
+{
+    internal interface ICourse
+    {
+
+        void AddGroup(Group group);
+        Group GetGroupByNo(string no);
+        Group[] Search(string search);
+
+    }
+}
---
+++ 14022023home/14022023home/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _14022023home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
